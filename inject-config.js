e4c355423b48ab73879a3db0644efc31d0d24228
Amber Moseri <aemoseri@my.yorku.ca>
2026-04-16 18:08:01 -0400

    updated inject-config.js

--- inject-config.js
+++ inject-config.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 const filePath = 'index.html';
 let html = fs.readFileSync(filePath, 'utf8');
 
-const apiUrl = process.env.FLOCK_PUBLIC_API_URL || process.env.FLOCK_API_URL || '';
+const apiUrl = process.env.FLOCK_PUBLIC_API_URL || '';
 html = html.replace(/__FLOCK_PUBLIC_API_URL__/g, apiUrl);
 
 fs.writeFileSync(filePath, html, 'utf8');
